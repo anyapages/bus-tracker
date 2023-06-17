f64e79f61d555227293fb20d3b990706eb8febc9
Anya C. Paranya <80136592+anyapages@users.noreply.github.com>
2023-06-17 16:53:47 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
 # MIT Bus Tracker 🚌
 
-MIT bus tracker is a  sample real-time bus tracking information that allows riders to track the exact location of a bus in real time around Massachusetts Institute of Technology (MIT).
+MIT Bus Tracker is a real-time bus tracking application that allows users to track the exact location of buses in real-time in Boston.
 
 ## Installation
-- Get [Google's Maps JavaScript API key](https://developers.google.com/maps/documentation/javascript/get-api-key) for development.
-- Add *mybusA.png* and *mybusB.png* markers with `function getIcon(bus)` on the map to highlight the bus routes.
 
-## Usage 
-<img src = 'example.png' width="340" height="290"> 
+To use the MIT Bus Tracker, follow these steps:
+
+1. Obtain a [Google Maps JavaScript API key](https://developers.google.com/maps/documentation/javascript/get-api-key) for development purposes.
+2. Get an API key from [MBTA API](https://api-v3.mbta.com).
+3. Add the `mybusA.png` and `mybusB.png` markers to the project. These markers will be used to highlight the bus routes on the map.
+
+## Usage
+
+![Example](example.png)
+
+The MIT Bus Tracker allows users to visualise the real-time location of buses on a map. Users can track the movement of buses and plan their journeys accordingly.
 
 ## Support
-[Maps JavaScript API](https://developers.google.com/maps/support/?hl=en_G)
+
+For any issues or questions, please refer to the [Maps JavaScript API documentation](https://developers.google.com/maps/support/?hl=en_G).
 
 ## Roadmap
-- Provide GPS public bus tracking information for cities in South East Asia.
-- Create functions and buttons for riders.
-- Launch a city bus tracking application.
 
-## Authors and acknowledgment
-- Bus data provided by Massachusetts Bay Transportation Authority (MBTA),
-- MIT xPro
+The MIT Bus Tracker has the following roadmap:
+
+- Expand the application to provide GPS-based public bus tracking information for cities in South East Asia.
+- Implement additional functions and buttons to enhance the user experience.
+- Launch a comprehensive city bus tracking application encompassing multiple cities and regions.
+
+## Authors and Acknowledgment
+
+The MIT Bus Tracker is developed as part of the MIT xPro programme. The bus data is provided by the Massachusetts Bay Transportation Authority (MBTA).
 
 ## License
-[MIT](https://github.com/anyapages/bus-tracker/blob/main/LICENSE)
 
+This project is licensed under the [MIT License](https://github.com/anyapages/bus-tracker/blob/main/LICENSE).
--- mapanimation.js
+++ mapanimation.js
@@ -1,100 +1,78 @@
-// Bus tracker function
-var map;
-var markers = [];
-            
- // load map
-function init(){
-    var myMap = {
-    zoom      : 14,
-    // centered at MIT
-    center    : { lat:42.36008201889161,lng:-71.09835377702795},
-    mapTypeId : google.maps.MapTypeId.ROADMAP
-};
-    var element = document.getElementById('map');
-    map = new google.maps.Map(element, myMap);
-    addMarkers();
-}
-            
-// add bus markers to map
-async function addMarkers(){
-// get bus data
-    var locations = await getBusLocations();
-            
-// loop through data, add bus markers
-locations.forEach(function(bus){
-    var marker = getMarker(bus.id);		
-    if (marker){
-        moveMarker(marker,bus);
-    }
-    else{
-        addMarker(bus);
-    }
-});
+let map;
+let markers = [];
 
-console.log(new Date());
-setTimeout(addMarkers,15000);
-}
-            
-// request real-time bus data from Massachusetts Bay Transportation Authority (MBTA)
-async function getBusLocations(){
-    var url = 'https://api-v3.mbta.com/vehicles?filter[route]=1&include=trip';	
-    var response = await fetch(url);
-    var json     = await response.json();
-    return json.data;
+// Initialise the map, centered at MIT
+function initMap() {
+  const myMap = {
+    zoom: 14,
+    center: { lat: 42.36008201889161, lng: -71.09835377702795 },
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  const element = document.getElementById('map');
+  map = new google.maps.Map(element, myMap);
+  addMarkers();
 }
 
-function addMarker(bus){
-    var icon = getIcon(bus);
-    var marker = new google.maps.Marker({
-    position: {
-        lat: bus.attributes.latitude,
-        lng: bus.attributes.longitude
-    },
-        map: map,
-        icon: icon,
-        id: bus.id
-    });
-        markers.push(marker);
-    }
-            
-function getIcon(bus){
-// select icon based on bus direction
-    if (bus.attributes.direction_id === 0) {
-        return 'mybusA.png';
-    }
-    return 'mybusB.png';
-}
-            
-function moveMarker(marker,bus) {
-// change icon if bus has changed direction
-var icon = getIcon(bus);
-marker.setIcon(icon);
-            
-// move icon to new latitude/longitude
-marker.setPosition( {
-    lat: bus.attributes.latitude, 
-    lng: bus.attributes.longitude
-});
-}
-            
-function getMarker(id){
-    var marker = markers.find(function(item){
-        return item.id === id;
-    });
-    return marker;
-}
-window.onload = init;
+// Add bus markers to the map
+async function addMarkers() {
+  const locations = await getBusLocations();
 
-let counter = 0;
-function move() {
-    setTimeout(() => {
-        if (counter >= busStops.length) return;
-        marker.setLngLat(busStops[counter]);
-        counter++;
-        move();
-      }, 1000);
+  locations.forEach(bus => {
+    const marker = getMarker(bus.id);
+
+    if (marker) {
+      moveMarker(marker, bus);
+    } else {
+      addMarker(bus);
+    }
+  });
+
+  setTimeout(addMarkers, 15000); // Refresh markers every 15 seconds
 }
 
-if (typeof module !== 'undefined') {
-    module.exports = { move };
+// Request real-time bus data from Massachusetts Bay Transportation Authority (MBTA)
+async function getBusLocations() {
+  const url = 'https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap';
+  const response = await fetch(url);
+  const json = await response.json();
+  return json.data;
+}
+
+// Add a new marker for the bus
+function addMarker(bus) {
+  const icon = getIcon(bus);
+  const marker = new google.maps.Marker({
+    position: { lat: bus.attributes.latitude, lng: bus.attributes.longitude },
+    map,
+    icon,
+    id: bus.id
+  });
+
+  markers.push(marker);
+}
+
+// Determine the appropriate icon for the bus based on its direction
+function getIcon(bus) {
+  if (bus.attributes.direction_id === 0) {
+    return 'mybusA.png';
   }
+  return 'mybusB.png';
+}
+
+// Update the position and icon of an existing marker
+function moveMarker(marker, bus) {
+  const icon = getIcon(bus);
+  marker.setIcon(icon);
+  marker.setPosition({
+    lat: bus.attributes.latitude,
+    lng: bus.attributes.longitude
+  });
+}
+
+// Get the marker by its ID
+function getMarker(id) {
+  return markers.find(item => item.id === id);
+}
+
+window.onload = initMap;
